320bd9a9e2cd9c477352506227b3294177d2bedc
zgordon01 <zgordon01@aurora.edu>
2016-02-19 12:52:38 -0600

    Added libraries, navbar, and some test code for main page

--- main/js/controllers.js
+++ main/js/controllers.js
@@ -1,7 +1,14 @@
 var modobj = angular.module('login',[]);
-modobj.controller('inputformController', function($scope) {
-    $scope.validateCreds=function(){
-            $scope.test=true;
-        
-    };
-         });
+var modobj2 = angular.module('home',[]);
+modobj.controller('inputformController', function($scope){
+
+});
+modobj.controller('home', function($scope){
+	//POPULATE THESE WITH REST CALL
+	$scope.tickets = {'1000':{'subject': "computer iz broken", 'priority': "Urgent", 'status': "Fresh", 'stack':"Triage", 'id':"1000", 'spawned': "date/time"}, '1001':{'subject': "need a cord", 'priority': "Low Priority", 'status': "Fresh", 'stack':"Triage", 'id':"1001", 'spawned': "date/time"}};
+	
+	$scope.userickets = {'1004':{'subject': "computer iz broken", 'priority': "Urgent", 'status': "Open", 'stack':"", 'id':"1000", 'spawned': "date/time"}, '1009':{'subject': "help me please", 'priority': "Critical", 'status': "Open", 'stack':"", 'id':"1000", 'spawned': "date/time"}};
+    
+    
+
+});
