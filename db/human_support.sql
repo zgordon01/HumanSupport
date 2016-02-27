08bb6a0139308f3db2ca858616a46758fcc4ffac
zgordon01 <zgordon01@aurora.edu>
2016-02-27 00:29:55 -0600

    Logins, sessions, base routes, and database/server functional
    -Node server running with our database is working with our application
    -Logins fully functional and getting the user's id and type (need to
    change from id to session key)
    -Base routes now working on home.html based on what type of user logs in
    -Session timeout modal prompt every hour
    -Logouts working

--- db/human_support.sql
+++ db/human_support.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 02, 2016 at 09:32 PM
+-- Generation Time: Feb 27, 2016 at 12:10 AM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -30,7 +30,8 @@ CREATE TABLE IF NOT EXISTS `comments` (
 `comment_id` int(11) NOT NULL,
   `contents` varchar(500) NOT NULL,
   `date_sent` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `type_id` int(11) NOT NULL
+  `type_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -53,7 +54,8 @@ CREATE TABLE IF NOT EXISTS `comment_types` (
 CREATE TABLE IF NOT EXISTS `messages` (
 `message_id` int(11) NOT NULL,
   `contents` varchar(500) NOT NULL,
-  `date_sent` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `date_sent` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -67,8 +69,19 @@ CREATE TABLE IF NOT EXISTS `profiles` (
   `first_name` varchar(10) NOT NULL,
   `last_name` varchar(10) NOT NULL,
   `email` varchar(25) NOT NULL,
-  `phone_number` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `phone_number` varchar(25) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `profiles`
+--
+
+INSERT INTO `profiles` (`profile_id`, `first_name`, `last_name`, `email`, `phone_number`, `date_created`) VALUES
+(1, 'Sean', 'Long', 'yourawizardhary@gmail.com', '222-222-2222', '2016-02-09 21:24:30'),
+(2, 'Zach', 'Gordon', 'themasterplan@gmail.com', '333-333-3333', '2016-02-09 21:24:30'),
+(3, 'Joey', 'Cazone', 'jcazone01@gmail.com', '1231231234', '2016-02-25 19:49:44'),
+(4, 'Bret', 'Kills', '2skill4u@gmail.com', '3213214321', '2016-02-25 19:49:44');
 
 -- --------------------------------------------------------
 
@@ -78,14 +91,26 @@ CREATE TABLE IF NOT EXISTS `profiles` (
 
 CREATE TABLE IF NOT EXISTS `tickets` (
   `ticket_id` int(11) NOT NULL,
-  `employee_id` int(11) NOT NULL DEFAULT '1',
+  `employee_id` int(11) DEFAULT NULL,
   `requestor_id` int(11) NOT NULL,
   `status_id` int(11) DEFAULT '1',
   `priority_id` int(11) NOT NULL DEFAULT '1',
   `stack_id` int(11) NOT NULL DEFAULT '1',
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `subject` varchar(250) NOT NULL
+  `subject` varchar(50) NOT NULL,
+  `description` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tickets`
+--
+
+INSERT INTO `tickets` (`ticket_id`, `employee_id`, `requestor_id`, `status_id`, `priority_id`, `stack_id`, `date_created`, `subject`, `description`) VALUES
+(1234, NULL, 3, 1, 2, 1, '2016-02-25 20:02:05', 'The Computers are alive and are attacking!', '0'),
+(2222, NULL, 4, 1, 1, 1, '2016-02-25 20:02:05', 'Me need internet rope.', '0'),
+(12345, NULL, 3, 1, 1, 1, '2016-02-25 20:04:25', 'Computer wont turn on.', '0'),
+(55555, NULL, 4, 1, 1, 1, '2016-02-25 20:04:54', 'Monitor displays satanic symbols.', '0'),
+(66776, NULL, 3, 1, 1, 1, '2016-02-25 21:42:40', 'Help my compter is on fire', '0');
 
 -- --------------------------------------------------------
 
@@ -120,14 +145,16 @@ CREATE TABLE IF NOT EXISTS `tickets_2_messages` (
 CREATE TABLE IF NOT EXISTS `ticket_priority` (
 `priority_id` int(11) NOT NULL,
   `priority` varchar(25) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `ticket_priority`
 --
 
 INSERT INTO `ticket_priority` (`priority_id`, `priority`) VALUES
-(1, 'Unassigned');
+(1, 'Unassigned'),
+(2, 'Urgent'),
+(3, 'Semi-Important');
 
 -- --------------------------------------------------------
 
@@ -173,11 +200,22 @@ INSERT INTO `ticket_status` (`status_id`, `status`) VALUES
 
 CREATE TABLE IF NOT EXISTS `users` (
 `user_id` int(11) NOT NULL,
+  `user_key` varchar(25) NOT NULL,
   `username` varchar(25) NOT NULL,
   `password` varchar(25) NOT NULL,
   `type_id` int(11) NOT NULL,
   `profile_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_key`, `username`, `password`, `type_id`, `profile_id`) VALUES
+(1, '', 'slong01', 'test123', 1, 1),
+(2, '', 'zgordon01', '123test', 2, 2),
+(3, '', 'bretkillz', 'test123test', 3, 4),
+(4, '', 'jcazone123', '123test123', 3, 3);
 
 -- --------------------------------------------------------
 
@@ -188,7 +226,16 @@ CREATE TABLE IF NOT EXISTS `users` (
 CREATE TABLE IF NOT EXISTS `user_types` (
 `type_id` int(11) NOT NULL,
   `user_type` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `user_types`
+--
+
+INSERT INTO `user_types` (`type_id`, `user_type`) VALUES
+(1, 'admin'),
+(2, 'empolyee'),
+(3, 'user');
 
 --
 -- Indexes for dumped tables
@@ -198,7 +245,7 @@ CREATE TABLE IF NOT EXISTS `user_types` (
 -- Indexes for table `comments`
 --
 ALTER TABLE `comments`
- ADD PRIMARY KEY (`comment_id`), ADD KEY `fk_types_idx` (`type_id`);
+ ADD PRIMARY KEY (`comment_id`), ADD KEY `fk_types_idx` (`type_id`), ADD KEY `fk_user3_idx` (`user_id`);
 
 --
 -- Indexes for table `comment_types`
@@ -210,7 +257,7 @@ ALTER TABLE `comment_types`
 -- Indexes for table `messages`
 --
 ALTER TABLE `messages`
- ADD PRIMARY KEY (`message_id`);
+ ADD PRIMARY KEY (`message_id`), ADD KEY `fk_user4_idx` (`user_id`);
 
 --
 -- Indexes for table `profiles`
@@ -289,7 +336,7 @@ MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT;
 -- AUTO_INCREMENT for table `profiles`
 --
 ALTER TABLE `profiles`
-MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `tickets_2_comments`
 --
@@ -304,7 +351,7 @@ MODIFY `t2m_id` int(11) NOT NULL AUTO_INCREMENT;
 -- AUTO_INCREMENT for table `ticket_priority`
 --
 ALTER TABLE `ticket_priority`
-MODIFY `priority_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `priority_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `ticket_stack`
 --
@@ -319,12 +366,12 @@ MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `user_types`
 --
 ALTER TABLE `user_types`
-MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- Constraints for dumped tables
 --
@@ -333,7 +380,14 @@ MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT;
 -- Constraints for table `comments`
 --
 ALTER TABLE `comments`
-ADD CONSTRAINT `fk_types` FOREIGN KEY (`type_id`) REFERENCES `comment_types` (`type_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ADD CONSTRAINT `fk_types` FOREIGN KEY (`type_id`) REFERENCES `comment_types` (`type_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_user3` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+ADD CONSTRAINT `fk_user4` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `tickets`
--- main/js/controllers.js
+++ main/js/controllers.js
@@ -1,14 +1,120 @@
-var modobj = angular.module('login',[]);
-var modobj2 = angular.module('home',[]);
-modobj.controller('inputformController', function($scope){
+//landingapp module with routes
+angular.module('landingapp', ['ngRoute']).config(['$routeProvider', function($routeProvider){
+	$routeProvider.when('/admin', {
+		templateUrl: 'admin.html'
+	}).when('/tech',{
+		templateUrl : 'tech.html'
+	}).when('/users',{
+		templateUrl : 'users.html'
+	})
+}]).controller('navbar', function($scope, $http, $timeout, $location, $interval) {
+	//begins the timer for session timeouts. This entire block of code fires after X length of time
+	$scope.sess_timer = function(){$timeout(function(){
+		//toggles the modal to on once the time has passesd
+		$('#timeoutmodal').modal('toggle');
+		
+		//this fires after 30 seconds and invokes logout()
+		$timeout(function (){
+			$scope.logout();
+    }, 30000);
+		
+		//declares the modal display counter to start at 30
+		$scope.count=30;
+		//counts down for display on the modal
+		$scope.timer = $interval(function (){
+			$scope.count = $scope.count-1;
+  		}, 1000);
+		
+	}, 3600000);};
+	
+	//logout function invoked when navbar logout button is clicked
+	$scope.logout = function(){
+		sessionStorage.usrid = "";
+		sessionStorage.usrtype = "";
+		window.location = "index.html";
+	};
+	//invoked when the modal's "I'm Still Here!" button is clicked
+	$scope.stillHere = function(){
+		//toggles the modal to off once the button is clicked
+		$('#myModal').modal('toggle');
+		//cancels the modal's countdown interval
+		$interval.cancel($scope.timer);
+	}
+	//invoked when the page is initialized
+	$scope.init = function(){
+		//must have more security than this. protecting this page is a priority
+			if(sessionStorage.usrtype==="1"){
+				$location.url("admin");
+			}
+			else if(sessionStorage.usrtype==="2"){
+				$location.url("tech");
+			}
+			else if(sessionStorage.usrtype==="3"){
+				$location.url("users");
+			}
+			else{
+				window.location = "index.html";
+			}
+		//begin the session timer
+		$scope.sess_timer();
+	};
+	$scope.init();
+});
+
+
+//login page module
+angular.module('login', []).controller('logincontroller', function($scope, $http) {
+	$scope.valid = true;
+    $scope.loading=false;
+    $scope.username="";
+	$scope.pass="";
+	//auth function invoked when button clicked
+	$scope.auth = function(){
+		$scope.loading=true;
+		var request = $http({
+		  method : 'POST',
+		  url    : 'http://198.211.99.235:8080/v1/login',
+		  data : $.param({username : $scope.username, password : $scope.pass}),
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+		});
+
+    request.success(function (data){
+		$scope.loading=false;
+      	$scope.contents = data;
+		//if the credentials are valid, store the user's id and user's type in session storage
+		if(data.valid == "yes"){
+			sessionStorage.usrid = data.usrid;
+			sessionStorage.usrtype = data.usrtype;
+			window.location = "home.html";
+		}
+		else{
+			$scope.valid=false;
+		}
+    });}
 
 });
-modobj.controller('home', function($scope){
-	//POPULATE THESE WITH REST CALL
+
+
+/*angular.module('home', []).controller('homecontroller', function($scope, $http){
+	//POPULATE THESE WITH REST CALL.. FORMULATE REST CALL IN NODE TO MODEL THIS DATA STRUCTURE
 	$scope.tickets = {'1000':{'subject': "computer iz broken", 'priority': "Urgent", 'status': "Fresh", 'stack':"Triage", 'id':"1000", 'spawned': "date/time"}, '1001':{'subject': "need a cord", 'priority': "Low Priority", 'status': "Fresh", 'stack':"Triage", 'id':"1001", 'spawned': "date/time"}};
 	
 	$scope.userickets = {'1004':{'subject': "computer iz broken", 'priority': "Urgent", 'status': "Open", 'stack':"", 'id':"1000", 'spawned': "date/time"}, '1009':{'subject': "help me please", 'priority': "Critical", 'status': "Open", 'stack':"", 'id':"1000", 'spawned': "date/time"}};
-    
-    
+});*/
 
-});
+
+/*
+function query(url, method, params) {
+        $scope.auth = function(url, method, params){
+		var request = $http({
+		  method : method,
+		  url    : url,
+		  data : $.param(params),
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+		});
+
+    request.success(function (data){
+		return data;
+    });}
+    }
+	*/
